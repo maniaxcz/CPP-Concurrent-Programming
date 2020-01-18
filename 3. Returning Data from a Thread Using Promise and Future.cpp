9f07d458c306b47c639f4db92ed293f347e5e41a
Manan Sharma <53045159+maniaxcz@users.noreply.github.com>
2020-01-18 19:05:45 +0530

    Update 3. Returning Data from a Thread Using Promise and Future.cpp

--- 3. Returning Data from a Thread Using Promise and Future.cpp
+++ 3. Returning Data from a Thread Using Promise and Future.cpp
@@ -4,7 +4,7 @@
 #include <memory>
 #include <future>
 
-void fun(std::promise<std::string> && pr)
+void fun(std::promise<std::string> && pr)                           // Taking Promise as an Argument
 {
     std::cout<<"\nWorker Thread Sending";
     pr.set_value("\nMessage From Thread");
@@ -13,10 +13,10 @@ void fun(std::promise<std::string> && pr)
 
 int main()
 {
-    std::promise<std::string> pr;
-    std::future<std::string> fut = pr.get_future();
+    std::promise<std::string> pr;                                   // Step 1 : Creating a Promise
+    std::future<std::string> fut = pr.get_future();                 // Step 2 : Creating a Future
 
-    std::thread th(fun,std::move(pr));
+    std::thread th(fun,std::move(pr));                              // Step 3 : Thread Created and Promise passed using move function
     std::cout<<"\nMain Receiving....";
     std::string str = fut.get();
     std::cout<<str;
